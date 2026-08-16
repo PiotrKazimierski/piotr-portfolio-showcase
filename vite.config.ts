4c57ce2edef1898bc015fa6f72bfae12c056ed2f
PiotrKazimierski <piotr.kazimierski@interia.com>
2026-08-16 19:46:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
-    prerender: {
-      enabled: true,
-      crawlLinks: true,
-    },
+    spaMode: true,
   },
-  nitro: {
-    preset: "static",
-  },
-  // Wskazanie bazowej ścieżki (jeśli strona działa pod piotrkazimierski.github.io/piotr-portfolio-showcase/)
-  // Jeśli korzystasz z własnej domeny podpiętej do GH Pages, zmień na: base: "/"
-  base: process.env.NODE_ENV === "production" ? "/piotr-portfolio-showcase/" : "/",
+  // Ustawienie nazwy repozytorium jako ścieżki bazowej dla GitHub Pages
+  // (Jeśli podepniesz własną domenę piotrkazimierski.pl, zmień poniżej na '/')
+  base: "/piotr-portfolio-showcase/",
 });
