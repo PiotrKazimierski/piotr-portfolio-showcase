b6fd323944316ede37ae7106e53cd126fbe6e0f0
PiotrKazimierski <piotr.kazimierski@interia.com>
2026-08-16 19:50:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ export default defineConfig({
   tanstackStart: {
     spaMode: true,
   },
-  // Ustawienie nazwy repozytorium jako ścieżki bazowej dla GitHub Pages
-  // (Jeśli podepniesz własną domenę piotrkazimierski.pl, zmień poniżej na '/')
-  base: "/piotr-portfolio-showcase/",
+  base: "/",
 });
